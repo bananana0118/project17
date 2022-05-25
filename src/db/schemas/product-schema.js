e2fe73cbce254c862a77c7680a0f6b509343db57
[SW2/서울/1반]심주용 <thelibra001@naver.com>
2022-05-25 14:52:14 +0900

    front 디자인 Add

--- src/db/schemas/product-schema.js
+++ src/db/schemas/product-schema.js
@@ -1,32 +1,30 @@
-import { Schema } from 'mongoose';
+import { Schema } from "mongoose";
 
-const ProductSchema = new Schema(
-  {
+const ProductSchema = new Schema({
     productName: {
-      type: String,
-      required: true,
+        type: String,
+        required: true,
     },
     productPrice: {
-      type: String,
-      required: true,
+        type: String,
+        required: true,
     },
     productSize: {
-      type: String,
-      required: true,
+        type: String,
+        required: true,
     },
     productDescription: {
-      type: String,
-      required: false,
+        type: String,
+        required: false,
     },
     productCategory: {
-      type: String,
-      required: true,
+        type: String,
+        required: true,
     },
     productManufacturer: {
         type: String,
         required: true,
     },
-}
-);
+});
 
 export { ProductSchema };
--- src/services/product-service.js
+++ src/services/product-service.js
@@ -1,27 +1,24 @@
-import { productModel } from '../db';
-
+import { productModel } from "../db";
 
 class ProductService {
-  // 본 파일의 맨 아래에서, new UserService(userModel) 하면, 이 함수의 인자로 전달됨
-  constructor(productModel) {
-    this.productModel = productModel;
-  }
+    // 본 파일의 맨 아래에서, new UserService(userModel) 하면, 이 함수의 인자로 전달됨
+    constructor(productModel) {
+        this.productModel = productModel;
+    }
 
-  // 회원가입
-  async addProduct(productInfo) {
+    // 상품 추가
+    async addProduct(productInfo) {
+        // db에 저장
+        const createdNewProduct = await this.productModel.create(productInfo);
 
-    // db에 저장
-    const createdNewProduct = await this.productModel.create(productInfo);
-
-    return createdNewProduct;
-  }
+        return createdNewProduct;
+    }
 
     // 상품 목록을 받음.
     async getProducts() {
-      const products = await this.productModel.findAll();
-      return products;
+        const products = await this.productModel.findAll();
+        return products;
     }
-  
 }
 
 const productService = new ProductService(productModel);
