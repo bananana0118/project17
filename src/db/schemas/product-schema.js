f5a6526dca7769da866dfa2833754a5af2004f66
[SW2/서울/1반]심주용 <thelibra001@naver.com>
2022-05-26 14:54:10 +0900

    add img URL input

---
+++ src/db/schemas/product-schema.js
@@ -0,0 +1,35 @@
+import { Schema, Types } from "mongoose";
+
+const ProductSchema = new Schema({
+    productName: {
+        type: String,
+        required: true,
+    },
+    productPrice: {
+        type: String,
+        required: true,
+    },
+    productSize: {
+        type: String,
+        required: true,
+    },
+    productDescription: {
+        type: String,
+        required: false,
+    },
+    productCategory: {
+        type: String,
+        // ref: "CateogrySchema",
+        required: true,
+    },
+    productManufacturer: {
+        type: String,
+        required: true,
+    },
+    productImg: {
+        type: String,
+        required: false,
+    },
+});
+
+export { ProductSchema };
