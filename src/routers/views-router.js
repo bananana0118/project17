2c743a4e3e46fb40e1117cece8349460a8a855a9
YeaJeong <bananana0118@gmail.com>
2022-05-28 10:05:12 +0900

    feat:사용자 수정기능 이름, 비밀번호 수정가능  구현

--- src/routers/views-router.js
+++ src/routers/views-router.js
@@ -10,7 +10,7 @@ viewsRouter.use("/", serveStatic("home"));
 viewsRouter.use("/register", serveStatic("register"));
 viewsRouter.use("/login", serveStatic("login"));
 viewsRouter.use("/product", serveStatic("product"));
-viewsRouter.use("/profile", serveStatic("profile"));
+viewsRouter.use("/testProfile", serveStatic("testProfile"));
 
 // views 폴더의 최상단 파일인 rabbit.png, api.js 등을 쓸 수 있게 함
 viewsRouter.use("/", serveStatic(""));
--- src/services/profile-service.js
+++ src/services/profile-service.js
@@ -1,28 +1,71 @@
-import { UserModel } from "../db";
+import { userModel } from "../db";
 
-import { profileService, userService } from "../services";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
 class ProfileService {
   // 본 파일의 맨 아래에서, new UserService(userModel) 하면, 이 함수의 인자로 전달됨
-  constructor(UserModel) {
-    this.UserModel = UserModel;
+  constructor(userModel) {
+    this.userModel = userModel;
   }
 
   //유저정보를 출력
   async getProfile() {
-    const myProfile = await this.UserModel.find({});
+    const myProfile = await this.userModel.find({});
     return myProfile;
   }
 
-  //   async deleteUser(userInfo) {
-  //     const { userEmail, password } = userInfo;
+  //우선 이메일로 해둠
+  // 유저정보 수정, 현재 비밀번호가 있어야 수정 가능함.
+  async setProfile(userInfoRequired, toUpdate) {
+    // 객체 destructuring
+    const { email, fullName, password, passwordConfirm } = userInfoRequired;
 
-  //     //user비밀번호가 일치하나 검증하고
-  //     // 삭제함
-  //     //문제가 있을경우 비밀번호가 다릅니다 라는 말 리턴
-  //     let user = await this.UserModel.findById(userEmail);
-  //   }
+
+    // 우선 해당 id의 유저가 db에 있는지 확인
+    let user = await this.userModel.findByEmail(email);
+
+    // db에서 찾지 못한 경우, 에러 메시지 반환
+    if (!user) {
+      throw new Error("가입 내역이 없습니다. 다시 한 번 확인해 주세요.");
+    }
+
+    // 이제, 정보 수정을 위해 사용자가 입력한 비밀번와 비밀번호 확인이 일치하는지 확인
+
+    if (!(password === passwordConfirm)) {
+      throw new Error(
+        "현재 비밀번호가 일치하지 않습니다. 다시 한 번 확인해 주세요."
+      );
+    }
+
+    // 이제 드디어 업데이트 시작
+
+    // 비밀번호도 변경하는 경우에는, 회원가입 때처럼 해쉬화 해주어야 함.
+
+    if (password) {
+      const newPasswordHash = await bcrypt.hash(password, 10);
+      toUpdate.password = newPasswordHash;
+    }
+
+    // 업데이트 진행
+    user = await this.userModel.updateByEmail({
+      email,
+      update: toUpdate,
+    });
+
+    return user;
+  }
+
+  async deleteUser(userInfo) {
+    const { userEmail, password } = userInfo;
+
+    //user비밀번호가 일치하나 검증하고
+    // 삭제함
+    //문제가 있을경우 비밀번호가 다릅니다 라는 말 리턴
+    let user = await this.userModel.findById(userEmail);
+  }
 }
 
-const profileService = new ProfileService(UserModel);
+const profileService = new ProfileService(userModel);
 
 export { profileService };
