f560edefc507edfa83dedf7eaa4808827ea5020c
YeaJeong <bananana0118@gmail.com>
2022-05-30 03:55:08 +0900

    feat: delete시 비밀번호 요청 구현

--- src/views/profile/profile.js
+++ src/views/profile/profile.js
@@ -19,72 +19,74 @@ const passwordConfirmInput = document.querySelector("#passwordConfirmInput");
 //GET: 사용자 정보가져오기
 //===================================================
 const getAcountInfo = async function () {
-    const user = await Api.get("/api/profile/myProfile");
-    console.log(user);
-    fullNameInput.value = user.fullName;
-    emailInput.value = user.email;
+  const user = await Api.get("/api/profile/myProfile");
+  console.log(user);
+  fullNameInput.value = user.fullName;
+  emailInput.value = user.email;
 };
 
 // ================================================
 // PATCH : 사용자 정보 수정 (오류 생길 수 있음)
 // ================================================
 const updateAccountInfo = async function (e) {
-    e.preventDefault();
-    const currentPassword = prompt("현재 비밀번호를 입력하세요");
-    const fullName = fullNameInput.value;
-    const email = emailInput.value;
-    const password = passwordInput.value;
-    const passwordConfirm = passwordConfirmInput.value;
-    // const mobileNumber = mobileNumberInput.value;
-    // const address = addressInput.value;
+  e.preventDefault();
+  const currentPassword = prompt("현재 비밀번호를 입력하세요");
+  const fullName = fullNameInput.value;
+  const email = emailInput.value;
+  const password = passwordInput.value;
+  const passwordConfirm = passwordConfirmInput.value;
+  // const mobileNumber = mobileNumberInput.value;
+  // const address = addressInput.value;
 
-    // 잘 입력했는지 확인
-    const isFullNameValid = fullName.length >= 2;
-    const isEmailValid = validateEmail(email);
-    const isPasswordValid = password.length >= 4;
-    const isPasswordSame = password === passwordConfirm;
+  // 잘 입력했는지 확인
+  const isFullNameValid = fullName.length >= 2;
+  const isEmailValid = validateEmail(email);
+  const isPasswordValid = password.length >= 4;
+  const isPasswordSame = password === passwordConfirm;
 
-    if (!isFullNameValid || !isPasswordValid) {
-        return alert("이름은 2글자 이상, 비밀번호는 4글자 이상이어야 합니다.");
-    }
+  if (!isFullNameValid || !isPasswordValid) {
+    return alert("이름은 2글자 이상, 비밀번호는 4글자 이상이어야 합니다.");
+  }
 
-    if (!isEmailValid) {
-        return alert("이메일 형식이 맞지 않습니다.");
-    }
+  if (!isEmailValid) {
+    return alert("이메일 형식이 맞지 않습니다.");
+  }
 
-    if (!isPasswordSame) {
-        return alert("비밀번호가 일치하지 않습니다.");
-    }
+  if (!isPasswordSame) {
+    return alert("비밀번호가 일치하지 않습니다.");
+  }
 
-    try {
-        const data = {
-            fullName,
-            email,
-            password,
-            passwordConfirm,
-            currentPassword,
-        };
-        const userUpdate = await Api.patch("/api/profile/edit", "", data);
-        console.log(userUpdate);
-        alert("정보 수정 완료");
-    } catch (err) {
-        console.error(err.stack);
-        alert(`문제 발생: ${err.message}`);
-    }
+  try {
+    const data = {
+      fullName,
+      email,
+      password,
+      passwordConfirm,
+      currentPassword,
+    };
+    const userUpdate = await Api.patch("/api/profile/edit", "", data);
+    console.log(userUpdate);
+    alert("정보 수정 완료");
+  } catch (err) {
+    console.error(err.stack);
+    alert(`문제 발생: ${err.message}`);
+  }
 };
 
 // ===================================================
 // DELETE:사용자 정보 지우기
 // ===================================================
-
 const deleteAccount = async function (e) {
-    e.preventDefault();
-    await Api.delete("/api/profile/quit");
+  e.preventDefault();
+  const confirm = window.confirm("정말 탈퇴하시나요?");
+  if (confirm) {
+    const password = prompt("비밀번호를 입력해주세요");
+    await Api.delete("/api/profile/quit", "", { password: password });
     alert("회원 탈퇴 완료");
     sessionStorage.removeItem("token");
     location.href = "/";
+  }
 };
-
 profileDelete.addEventListener("click", deleteAccount);
 getAcountInfo();
 
