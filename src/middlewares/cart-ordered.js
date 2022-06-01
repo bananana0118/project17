fca46be55ca631c85d0083193efefecb2b719991
YeaJeong <bananana0118@gmail.com>
2022-06-01 18:17:09 +0900

    Test

---
+++ src/middlewares/cart-ordered.js
@@ -0,0 +1,27 @@
+import { productService, userService } from "../services";
+
+async function cartOrdered(req, res, next) {
+    //유저 objectId가져오기
+    const userId = req.currentUserId;
+    const user = await userService.getUser(userId);
+
+    //prodjct id 가져오기
+    const cartData = req.body;
+    let orderedProduct = [];
+
+    for (let i = 0; i < cartData.length; i++) {
+        console.log("CartData " + cartData[i]._id);
+        let orderData = await productService.getProductById(cartData[i]._id);
+        orderedProduct.push(orderData._id);
+    }
+
+    const orderInfo = {
+        userId: user._id,
+        orderProduct: orderedProduct,
+    };
+
+    req.orderInfo;
+    next();
+}
+
+export { cartOrdered };
