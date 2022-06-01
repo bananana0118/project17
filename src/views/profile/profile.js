755f3bd27dd608f9d27fee7b184caffadb82218e
grey <kdc4925123@gmail.com>
2022-06-01 15:26:36 +0900

    계정관리 세부수정 휴대폰번호 추가

--- src/views/profile/profile.js
+++ src/views/profile/profile.js
@@ -13,7 +13,7 @@ const emailInput = document.querySelector("#emailInput");
 const passwordInput = document.querySelector("#passwordInput");
 const passwordConfirmInput = document.querySelector("#passwordConfirmInput");
 const greeting = document.querySelector(".greetings");
-// const mobileNumberInput = document.querySelector("#mobileNumber");
+const phoneNumberInput = document.querySelector("#phoneNumber");
 // const addressInput = document.querySelector("#address");
 
 // =================================================
@@ -22,9 +22,9 @@ const greeting = document.querySelector(".greetings");
 
 const getAcountInfo = async function () {
     const user = await Api.get("/api/profile/myProfile");
-    console.log(user);
     fullNameInput.value = user.fullName;
     emailInput.value = user.email;
+    if (user.phoneNumber) phoneNumberInput.value = user.phoneNumber; // 이야기 해야함. 스키마 설정
     greeting.innerHTML = `${user.fullName}님 환영합니다!`;
 };
 
@@ -32,14 +32,13 @@ const getAcountInfo = async function () {
 // PATCH : 사용자 정보 수정 (오류 생길 수 있음)
 // ================================================
 const updateAccountInfo = async function (e) {
-
     e.preventDefault();
     const currentPassword = prompt("현재 비밀번호를 입력하세요");
     const fullName = fullNameInput.value;
     const email = emailInput.value;
     const password = passwordInput.value;
     const passwordConfirm = passwordConfirmInput.value;
-    // const mobileNumber = mobileNumberInput.value;
+    const phoneNumber = phoneNumberInput.value;
     // const address = addressInput.value;
 
     // 잘 입력했는지 확인
@@ -48,16 +47,25 @@ const updateAccountInfo = async function (e) {
     const isPasswordValid = password.length >= 4;
     const isPasswordSame = password === passwordConfirm;
 
-    if (!isFullNameValid || !isPasswordValid) {
-        return alert("이름은 2글자 이상, 비밀번호는 4글자 이상이어야 합니다.");
+    if (fullName) {
+        if (!isFullNameValid) {
+            return alert("이름은 2글자 이상이어야 합니다.");
+        }
     }
 
-    if (!isEmailValid) {
-        return alert("이메일 형식이 맞지 않습니다.");
+    if (email) {
+        if (!isEmailValid) {
+            return alert("이메일 형식이 맞지 않습니다.");
+        }
     }
 
-    if (!isPasswordSame) {
-        return alert("비밀번호가 일치하지 않습니다.");
+    if (password) {
+        if (!isPasswordValid) {
+            return alert("비밀번호는 4글자 이상이어야 합니다.");
+        }
+        if (!isPasswordSame) {
+            return alert("비밀번호가 일치하지 않습니다.");
+        }
     }
 
     try {
@@ -66,16 +74,16 @@ const updateAccountInfo = async function (e) {
             email,
             password,
             passwordConfirm,
+            phoneNumber,
             currentPassword,
         };
         const userUpdate = await Api.patch("/api/profile/edit", "", data);
         console.log(userUpdate);
-        alert("정보 수정 완료");
+        alert("계정 정보가 수정되었습니다.");
     } catch (err) {
         console.error(err.stack);
         alert(`문제 발생: ${err.message}`);
     }
-
 };
 
 // ===================================================
