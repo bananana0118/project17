94a33a3c9ec2b2f7cbc6398c0020523f62283862
LYJ <rbdnjs251@naver.com>
2022-06-03 01:39:57 +0900

    단일 아이템 구매시 결제페이지 이동 수정

--- src/views/payment/payment.js
+++ src/views/payment/payment.js
@@ -14,10 +14,11 @@ selectedEmail.addEventListener('change', changeDomain);
 let sumPrice = 0;
 
 // 더미데이터 -> 장바구니 데이터로 교체 예정 
-let orderItems = !localStorage.getItem('buyOne') 
+let orderItems = ! localStorage.getItem('buyOne') 
                  ? JSON.parse(localStorage.getItem('cart'))
                  : JSON.parse(localStorage.getItem('buyOne'));
 
+localStorage.removeItem('buyOne');
 const register = async () => {
     const orderProduct = orderItems;
     console.log(orderProduct)
